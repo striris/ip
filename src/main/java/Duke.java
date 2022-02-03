ce07e46a2f14edaa02d41596efc2325aa042a789
striris <72813713+striris@users.noreply.github.com>
2022-02-04 01:34:13 +0800

    Level 4. ToDos, Events, Deadlines Add support for tracking three types of tasks: ToDos: tasks without any date/time attached to it e.g., visit new theme park Deadlines: tasks that need to be done before a specific date/time e.g., submit report by 11/10/2019 5pm Events: tasks that start at a specific time and ends at a specific time e.g., team project meeting on 2/10/2019 2-4pm

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,6 +4,8 @@ public class Duke {
 
     public static void main(String[] args) {
         Task[] tasks = new Task[100];
+        String by = new String();
+        String at = new String();
         int count = 0;
         Scanner sc = new Scanner(System.in);
         String greeting = "----------------------------------------------\n"
@@ -14,36 +16,66 @@ public class Duke {
         System.out.println(greeting);
 
         while(true){
-            String input = sc.nextLine();
+            String userInput = sc.nextLine();
             System.out.println("----------------------------------------------");
-            if(input.equals("bye")){
+            if(userInput.equals("bye")){
                 System.out.println(bye);
                 break;
             }
-            else if(input.equals("list")){
+            else if(userInput.equals("list")){//show list
                 System.out.println("Here are the tasks in your list:");
                 for(int i = 0; i<count ; i++)
-                    System.out.println((i + 1) + ".[" + tasks[i].getStatusIcon() + "] " + tasks[i].description);
+                    System.out.println(i+1 + "." + tasks[i]);
             }
-            else if(input.split(" ")[0].equals("mark")){
+            else if(userInput.split(" ")[0].equals("mark")){//mark task
                 //get the integer k in the string
                 System.out.println("Nice! I've marked this task as done:");
-                int k = Integer.parseInt(input.split(" ")[1]);
+                int k = Integer.parseInt(userInput.split(" ")[1]);
                 tasks[k-1].markAsDone();
-                System.out.println(k + ".[" + tasks[k-1].getStatusIcon() + "] " + tasks[k-1].description);
+                System.out.println(tasks[k-1]);
             }
-            else if(input.split(" ")[0].equals("unmark")){
+            else if(userInput.split(" ")[0].equals("unmark")){//unmark task
                 //get the integer k in the string
                 System.out.println("OK, I've marked this task as not done yet:");
-                int k = Integer.parseInt(input.split(" ")[1]);
+                int k = Integer.parseInt(userInput.split(" ")[1]);
                 tasks[k-1].markAsNotDone();
-                System.out.println(k + ".[" + tasks[k-1].getStatusIcon() + "] " + tasks[k-1].description);
+                System.out.println(tasks[k-1]);
             }
-            else{
-                System.out.println("added: " + input);
-                Task t = new Task(input);
+            else if(userInput.startsWith("todo")){
+                String toDoDescription = userInput.split("todo")[1].trim();
+                Task t = new ToDo(toDoDescription);
                 tasks[count] = t;
                 count++;
+                System.out.println("Got it! I've added this task: ");
+                System.out.println(t);
+                System.out.println("Now you have " + count + " tasks in the list.");
+            }
+            else if(userInput.split(" ")[0].equals("deadline")){//add deadline
+                //split description and time
+                String[] input = userInput.split("/by");
+                by = input[1].trim();
+                String deadlineDescription = input[0].split("deadline")[1].trim();
+                Task d = new Deadline(deadlineDescription, by);
+                tasks[count] = d;
+                count++;
+                System.out.println("Got it! I've added this task: ");
+                System.out.println(d);
+                System.out.println("Now you have " + count + " tasks in the list.");
+            }
+            else if(userInput.split(" ")[0].equals("event")){//add deadline
+                //split description and time
+                String[] input = userInput.split("/at");
+                at = input[1].trim();
+                String eventDescription = input[0].split("event")[1].trim();
+                Task e = new Event(eventDescription, at);
+                tasks[count] = e;
+                count++;
+                System.out.println("Got it! I've added this task: ");
+                System.out.println(e);
+                System.out.println("Now you have " + count + " tasks in the list.");
+            }
+            else{
+                System.out.println("I don't understand D:");
             }
             System.out.println("----------------------------------------------");
         }
